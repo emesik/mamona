6f0da09c4838e63851dd578634bedacc15849d8b
Michał Sałaban <michal@salaban.info>
2013-01-06 08:32:05 +0100

    Clean up distutils/setuptools mess

--- setup.py
+++ setup.py
@@ -1,42 +1,27 @@
+# -*- coding: utf-8 -*-
 from distutils.core import setup
-import os
+from setuptools import find_packages
 
-# Package compilation taken from django-registration:
-# http://bitbucket.org/ubernostrum/django-registration/src/tip/setup.py
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir:
-    os.chdir(root_dir)
+PACKAGES = ['mamona', 'mamona.backends', 'mamona.backends.dummy', 'mamona.backends.paypal']
 
-for dirpath, dirnames, filenames in os.walk('mamona'):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        pkg = dirpath.replace(os.path.sep, '.')
-        if os.path.altsep:
-            pkg = pkg.replace(os.path.altsep, '.')
-        packages.append(pkg)
-    elif filenames:
-        prefix = dirpath[13:]
-        for f in filenames:
-            data_files.append(os.path.join(prefix, f))
-
-setup(name="mamona",
-      version="0.1",
-      description="Fully portable Django payments application",
-      author="Michal Salaban",
-      author_email="",
-      install_requires = ['setuptools'],
-      url="http://michal.salaban.info",
-      package_dir={'mamona': 'mamona'},
-      packages=packages,
-      package_data={'mamona': data_files}, 
-      classifiers=["Environment :: Web Environment",
-                   "Intended Audience :: Developers",
-                   "License :: OSI Approved :: BSD License",
-                   "Operating System :: OS Independent",
-                   "Programming Language :: Python",
-                   "Framework :: Django",])
+setup(
+	name="mamona",
+	version="0.1",
+	description="Fully portable Django payments application",
+	author=u"Michał Sałaban",
+	author_email="michal@salaban.info",
+	url="http://github.com/emesik/mamona",
+#	package_dir={'mamona': 'mamona'},
+	packages=PACKAGES,
+	include_package_data=True,
+#	package_data={'mamona': data_files}, 
+	classifiers=[
+		"Environment :: Web Environment",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: BSD License",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python",
+		"Framework :: Django",
+		],
+	zip_safe=False
+	)
