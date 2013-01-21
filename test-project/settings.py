08d36681c31c29340c6e6489868e960e5a47d939
Michał Sałaban <michal@salaban.info>
2013-01-21 19:32:31 +0100

    Catch up with the world, use new Django stuff

--- test-project/settings.py
+++ test-project/settings.py
@@ -55,9 +55,9 @@ SECRET_KEY = 'hdaw2jet30@z0sm+zl$y_+8vrem2-mih5)(e^d@ng8@6m6wfth'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-	'django.template.loaders.filesystem.load_template_source',
-	'django.template.loaders.app_directories.load_template_source',
-#	 'django.template.loaders.eggs.load_template_source',
+	'django.template.loaders.filesystem.Loader',
+	'django.template.loaders.app_directories.Loader',
+#	 'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
